d734450843490bf4643ef52212ce2cedc544ce0f
brunocozendey <brunocozendey@gmail.com>
2020-07-03 21:56:01 -0300

    Updates

---
+++ precache-manifest.ff239b421829a994ef993ea40dc924ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e99c3d3f454d6e2cb3a12c9d39c2f4",
+    "url": "/git-repo/index.html"
+  },
+  {
+    "revision": "765f779614bcd255bcad",
+    "url": "/git-repo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d2efce37b8633c8c3b5a",
+    "url": "/git-repo/static/js/2.aeb2c898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-repo/static/js/2.aeb2c898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765f779614bcd255bcad",
+    "url": "/git-repo/static/js/main.cbc379a5.chunk.js"
+  },
+  {
+    "revision": "c401581e623b2364c6d7",
+    "url": "/git-repo/static/js/runtime-main.09420da5.js"
+  }
+]);
